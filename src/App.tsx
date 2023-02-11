408e961700a01d44c08c493dd351d5a9307ba398
ryedough <adiwaskita@proton.me>
2023-02-11 15:31:07 +0700

    implemented route finder more than one

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import MazeBlock from './class/MazeBlock';
-import UseSolve from './hooks/UseSolve';
+import useSolve from './hooks/useSolve';
 
 function App() {
   let [blocks, setBlocks] = useState<MazeBlock[]>([]);
@@ -8,32 +8,70 @@ function App() {
   let [finish, setFinish] = useState(0);
   let [col, setCol] = useState(5);
   let [row, setRow] = useState(5);
-  let {solver, setBlock, route, clearRoute} = UseSolve(blocks, setBlocks, finish, start, col, row);
+  let [selected, setSelected] = useState<number | null>(null);
+  let [maxRoute, setMaxRoute] = useState(1);
+  let {solver, setBlock, routes, clearRoutes} = useSolve(blocks, setBlocks, finish, start, col, row, maxRoute);
 
-  useEffect(()=>{
+  //clear maze and init maze
+  const clear = ()=>{
+    setSelected(null);
     setBlocks(Array(col*row).fill(0).map((_, index)=>new MazeBlock()));
     setFinish(col*row-1);
     setStart(0);
-    clearRoute();
+    clearRoutes();
+  }
+
+  useEffect(()=>{
+    if(routes.length > 0) setSelected(0);
+    else setSelected(null);
+  }, [routes])
+
+
+  useEffect(()=>{
+    clear();
   }, [col, row])
 
   return (
     <div className="App flex flex-col w-max">
       <div className='relative'>
+        { selected !== null &&
         <div className='top-1 relative'>
-        {route.map((route, index)=>{
-          return <div key={index} className='absolute w-10 h-10 z-[1] pointer-events-none' style={{transform: `translate(${(route[0]%col)*48.5}px, ${(Math.floor(route[0]/col))*48.5}px)`}}>
-            <div className='relative w-full h-full'>
-              {route[1] === 'top' && <div className='absolute transform -translate-x-1/2 right-1/2 -bottom-2/3 w-1 h-[120%] bg-yellow-500'></div>}
-              {route[1] === 'left' && <div className='absolute transform -translate-y-1/2 top-1/2 -right-2/3 w-[130%] h-1 bg-yellow-500'></div>}
-              {route[1] === 'right' && <div className='absolute transform -translate-y-1/2 top-1/2 -left-2/3 w-[130%] h-1 bg-yellow-500'></div>}
-              {route[1] === 'bottom' && <div className='absolute transform -translate-x-1/2 right-1/2 -top-2/3 w-1 h-[120%] bg-yellow-500'></div>}
+          {routes[selected].map((route, index)=>{
+            return <div key={index} className='absolute w-10 h-10 z-[1] pointer-events-none' style={{transform: `translate(${(route[0]%col)*48.5}px, ${(Math.floor(route[0]/col))*48.5}px)`}}>
+              <div className='relative w-full h-full'>
+                {route[1] === 'top' && <div className='absolute transform -translate-x-1/2 right-1/2 -bottom-2/3 w-1 h-[120%] bg-yellow-500'></div>}
+                {route[1] === 'left' && <div className='absolute transform -translate-y-1/2 top-1/2 -right-[55%] w-[121%] h-1 bg-yellow-500'></div>}
+                {route[1] === 'right' && <div className='absolute transform -translate-y-1/2 top-1/2 -left-2/3 w-[130%] h-1 bg-yellow-500'></div>}
+                {route[1] === 'bottom' && <div className='absolute transform -translate-x-1/2 right-1/2 -top-2/3 w-1 h-[120%] bg-yellow-500'></div>}
+              </div>
+            </div>
+          })}
+        </div>
+        }
+
+        { routes.length > 0 &&
+          <div className='absolute top-0 left-[102%] w-max h-full flex flex-col items-center'>
+            <div className='w-full text-center bg-slate-600 py-2 text-white font-bold px-4'> Ada {routes.length} rute ditemukan </div>
+            <div className='bg-slate-200 grow p-2 w-full h-[10rem] overflow-auto'>
+              <ol className='text-white flex flex-col gap-y-2'>
+                {routes.map((route, index)=>{
+                  return (
+                    <li onClick={()=>setSelected(index)} className={'bg-blue-500 hover:bg-blue-700 flex rounded-md overflow-hidden cursor-pointer border-blue-500 hover:border-blue-700 border-2 ' + 
+                    (selected == index ? 'border-blue-700 bg-blue-700' : '')}>
+                      <div className='px-2 bg-white text-blue-500'>{index+1}</div>
+                      <div className='grow pl-1'>
+                        {route.length} langkah
+                      </div>
+                    </li>
+                )})}
+              </ol>
             </div>
           </div>
-        })}
-        </div>
-
-        {blocks.reduce((resultArray, item, index) => { 
+        }
+  
+        {
+        //seperate blocks into rows
+        blocks.reduce((resultArray, item, index) => { 
           const chunkIndex = Math.floor(index/col)
 
           if(!resultArray[chunkIndex]) {
@@ -43,7 +81,8 @@ function App() {
           resultArray[chunkIndex].push(item)
 
           return resultArray
-        }, [] as MazeBlock[][]).map((row, x)=>{
+        }, [] as MazeBlock[][])
+          .map((row, x)=>{
           return <div key={x} className="flex">
             {row.map((block, y)=>{
               return <>
@@ -77,19 +116,23 @@ function App() {
           </div>
         })}
       </div>
-      <button className='text-white bg-blue-500 p-2 rounded-md font-bold mt-5 w-11/12 self-center' onClick={()=>solver()}>
+      <button className='text-white bg-blue-500 p-2 rounded-md font-bold mt-5 w-11/12 self-center' onClick={()=>{setSelected(null);clearRoutes();solver()}}>
         Solve
       </button>
-      <button className='text-white bg-red-500 p-2 rounded-md font-bold mt-5 w-11/12 self-center' onClick={()=>{setBlocks(Array(col*row).fill(0).map((_, index)=>new MazeBlock())); clearRoute()}}>
+      <button className='text-white bg-red-500 p-2 rounded-md font-bold mt-5 w-11/12 self-center' onClick={()=>{clear()}}>
         Clear
       </button>
       <div className='w-11/12 self-center mt-4'>
-        <div className='text-black w-full'>Columns</div>
-        <input type='number' className='text-black p-2 rounded-md font-bold w-full border border-black' value={col} onFocus={(e)=>{e.target.blur()}} onChange={(e)=>{setCol(Number(e.target.value))}}/>
+        <div className='text-black w-full'>Kolom</div>
+        <input type='number' className='text-black p-2 rounded-md font-bold w-full border border-black' value={col} onChange={(e)=>{!e.target.value ? setCol(1) : setCol(Number(e.target.value))}}/>
       </div>
       <div className='w-11/12 self-center mt-4'>
-        <div className='text-black w-full'>row</div>
-        <input type='number' className='text-black p-2 rounded-md font-bold w-full border border-black' value={row} onFocus={(e)=>{e.target.blur()}} onChange={(e)=>{setRow(Number(e.target.value))}}/>
+        <div className='text-black w-full'>Baris</div>
+        <input type='number' className='text-black p-2 rounded-md font-bold w-full border border-black' value={row} onChange={(e)=>{!e.target.value ? setRow(1) :setRow(Number(e.target.value))}}/>
+      </div>
+      <div className='w-11/12 self-center mt-4'>
+        <div className='text-black w-full'>Maks. Rute yang dicari</div>
+        <input type='number' className='text-black p-2 rounded-md font-bold w-full border border-black' value={maxRoute} onChange={(e)=>{!e.target.value ? setMaxRoute(1) :setMaxRoute(Number(e.target.value))}}/>
       </div>
     </div>
   );
--- src/hooks/UseSolve.tsx
+++ src/hooks/UseSolve.tsx
@@ -4,61 +4,19 @@ type direction = 'top'|'left'|'right'|'bottom';
 const DIRECTION : direction[] = ['top', 'left', 'right', 'bottom']
 
 //note for [number, direction] tuple, symbolize = [current route , from which direction]
+//TODO: fix when index 0 cannot become candidate
 
-function UseSolve(blocks : MazeBlock[], setBlocks : React.Dispatch<React.SetStateAction<MazeBlock[]>>,finish: number, start: number, col: number, row: number){
+function useSolve(blocks : MazeBlock[], setBlocks : React.Dispatch<React.SetStateAction<MazeBlock[]>>,finish: number, start: number, col: number, row: number, maxRoute : number = 1){
     let visited = useRef<boolean[]>([]);
     let routeRef = useRef<[number, direction][]>([]);
-    let [route, setRoute] = useState<[number, direction][]>([]);
+    let routesRef = useRef<[number, direction][][]>([]);
+    let [routes, setRoutes] = useState<[number, direction][][]>([]); // array of route
     let flag = useRef(false);
 
-    const getCandidate = (index : number)=>{
-      let candidate : [number, direction][] = [];
-      DIRECTION.forEach(direct=>{
-        let adjacentBlock = getAdjacentBlock(index, direct)
-        if(blocks[index][direct] && adjacentBlock && !visited.current[adjacentBlock])
-          candidate.push([adjacentBlock, direct]);
-      })
-      return candidate;
-    }
-
-    const solver = ()=>{
-        flag.current = false;
+    const clearRoutes = ()=>{
+        routesRef.current = [];
         routeRef.current = [];
-        visited.current = Array(col*row).fill(false);
-        solve(start);
-        setRoute(routeRef.current);
-    }
-
-    useEffect(()=>{
-        route.forEach((route)=>console.log('current route : ', route[0], 'from : ', route[1]));
-    }, [route])
-
-    const solve = (index : number)=>{
-      const candidates = getCandidate(index);
-
-      visited.current[index] = true;
-
-      if(index === finish || flag.current){
-        flag.current = true;
-        return
-      }
-
-      candidates.forEach((candidate)=>{
-        if(flag.current)
-            return
-            
-        routeRef.current.push(candidate);
-        solve(candidate[0]);
-
-        if(flag.current)
-            return
-        
-        routeRef.current.pop();
-      })
-
-      if(flag.current)
-        return
-      visited.current[index] = false;
+        setRoutes([]);
     }
 
     const getAdjacentBlock = (index : number, direction : 'left' | 'right' | 'bottom' | 'top') : null | number =>{
@@ -98,10 +56,73 @@ function UseSolve(blocks : MazeBlock[], setBlocks : React.Dispatch<React.SetStat
       setBlocks(newBlocks);
     } 
 
-    const clearRoute = ()=>{
-        setRoute([]);
+    const getCandidate = (index : number)=>{
+      let candidate : [number, direction][] = [];
+      DIRECTION.forEach(direct=>{
+        let adjacentBlock = getAdjacentBlock(index, direct)
+        if(blocks[index][direct] && adjacentBlock !== null && !visited.current[adjacentBlock])
+          candidate.push([adjacentBlock, direct]);
+      })
+      return candidate;
     }
-    return {solver, setBlock, route, clearRoute};
+
+    // const printRoute = ()=>{
+    //   routes.forEach((route)=>{
+    //     let route2d = Array(row).fill(0).map(()=>Array(col).fill(0));
+    //     route.forEach((r)=>{
+    //       route2d[Math.floor(r[0]/col)][r[0]%col] = 1;
+    //     })
+    //     console.log(route2d);
+    //   });
+    // }
+    // useEffect(()=>{
+    //     printRoute();
+    // }, [routes])
+
+    const solver = ()=>{
+        flag.current = false;
+        routeRef.current = [];
+        routesRef.current = [];
+        visited.current = Array(col*row).fill(false);
+        solve(start);
+        setRoutes(routesRef.current.sort((a,b)=>a.length - b.length));
+    }
+
+    const solve = (index : number)=>{
+      const candidates = getCandidate(index);
+
+      visited.current[index] = true;
+
+      if(index === finish || flag.current){
+        if(index === finish){
+          routesRef.current.push([...routeRef.current]);
+        }
+
+        if(routesRef.current.length === maxRoute){
+          flag.current = true;
+        return
+       }
+      }
+
+      candidates.forEach((candidate)=>{
+        if(flag.current)
+            return
+            
+        routeRef.current.push(candidate);
+        solve(candidate[0]);
+
+        if(flag.current)
+            return
+        
+        routeRef.current.pop();
+      })
+
+      if(flag.current)
+        return
+      visited.current[index] = false;
+    }
+
+    return {solver, setBlock, routes, clearRoutes};
 }
 
-export default UseSolve;
+export default useSolve
